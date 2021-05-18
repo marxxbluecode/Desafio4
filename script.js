c868c5a0837fc7461cb1a4eec01e0363934cd7f7
marxxcodes <marxx171@gmail.com>
2021-05-18 16:10:33 -0300

    Desafio 4 Vnw

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1
+function Say_so(){
+    console.log('Didnt even notice')
+}
+Say_so()
+// 2
+function Say_my_name(name){
+    console.log(name)
+}
+Say_my_name('Marx')
+// 3
+function Feeling_myself(name, age, music){
+    console.log(name)
+    console.log(age)
+    console.log(music)
+}
+Feeling_myself('Marx', 23, 'eclético rsrs')
+// 4
+function I_love_it(movie, music){
+    console.log(movie)
+    console.log(music)
+}
+I_love_it('The Mitchells vs The Machines', 'Zankoku na Tenshi no Thesis')
+// 5
+function triple(number){
+    console.log(number *3)
+}
+triple(17)
